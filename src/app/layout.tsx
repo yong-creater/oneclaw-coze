b69333532a9790691a5542b5bbf176ed57a5c819
user4582638579 <1088981829427300-user4582638579@noreply.coze.cn>
2026-04-13 09:20:26 +0800

    fix: 百度统计ID写死到代码中

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -239,13 +239,12 @@ export default function RootLayout({
         />
         
         {/* 百度统计 */}
-        {baiduAnalyticsId && (
-          <Script
-            id="baidu-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                var _hmt = _hmt || [];
+        <Script
+          id="baidu-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _hmt = _hmt || [];
                 (function() {
                   var hm = document.createElement("script");
                   hm.src = "https://hm.baidu.com/hm.js?f43feef2e8e09bcc3bca6c5f485ac461";
