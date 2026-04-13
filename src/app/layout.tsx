50499fe221d41111055f6629032f37fca95487dd
user4582638579 <1088981829427300-user4582638579@noreply.coze.cn>
2026-04-13 10:06:51 +0800

    fix: 百度统计改为beforeInteractive尽早加载

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -238,22 +238,18 @@ export default function RootLayout({
           }}
         />
         
-        {/* 百度统计 */}
-        <Script
-          id="baidu-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _hmt = _hmt || [];
-                (function() {
-                  var hm = document.createElement("script");
-                  hm.src = "https://hm.baidu.com/hm.js?f43feef2e8e09bcc3bca6c5f485ac461";
-                  var s = document.getElementsByTagName("script")[0]; 
-                  s.parentNode.insertBefore(hm, s);
-                })();
-              `,
-            }}
-          />
+        {/* 百度统计 - beforeInteractive确保尽早加载以便被检测 */}
+        <Script id="baidu-analytics" strategy="beforeInteractive">
+          {`
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?f43feef2e8e09bcc3bca6c5f485ac461";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          `}
+        </Script>
         
 	        {/* Google Analytics */}
         {process.env.NEXT_PUBLIC_GA_ID && (
